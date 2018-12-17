46d257f56aa36ab24a7ad501ac9870bed3bc156f
André Silva <2493377+askpt@users.noreply.github.com>
2018-12-17 16:04:12 +0000

    Add project files.

---
+++ GridExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GridExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
